7b5a1bf0b8b9d1da9d7cd186240902d21b7f6446
OmBayus <omerbayramcavus@gmail.com>
2022-06-08 01:23:14 +0300

    buymeacoffee

--- src/components/Donate/Donate.jsx
+++ src/components/Donate/Donate.jsx
@@ -1,26 +1,38 @@
-import React from "react"
-import { useHistory } from "react-router"
-import {Container,Image} from "react-bootstrap"
-import Head from "./Head"
+import React from "react";
+import { useHistory } from "react-router";
+import { Container, Image } from "react-bootstrap";
+import Head from "./Head";
 
-import "./Donate.css"
+import "./Donate.css";
 
+const Donate = () => {
+  const history = useHistory();
 
-const Donate = ()=>{
-    const history = useHistory()
+  return (
+    <Container>
+      <Head title="Donation" text="Support me by donating." />
+      <div className="d-flex justify-content-center">
+        <div
+          className="selectPayment"
+          onClick={() =>
+            (window.location = "https://www.buymeacoffee.com/ombayus")
+          }
+        >
+          <div>
+            <Image src="/buymeacoffee.png" className="icon" />
+          </div>
+        </div>
+        <div
+          className="selectPayment"
+          onClick={() => history.push("/donate/metamask")}
+        >
+          <div>
+            <Image src="/metamask.webp" className="icon" />
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
+};
 
-    return(
-        <Container>
-            <Head title="Donation" text="Support me by donating."/>
-            <div className="d-flex justify-content-center">
-                <div className="selectPayment" onClick={()=>history.push("/donate/metamask")}>
-                    <div>
-                        <Image src="/metamask.webp" className="icon"/>
-                    </div>
-                </div>
-            </div>
-        </Container>
-    )
-}
-
-export default Donate
+export default Donate;
